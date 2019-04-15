30203ed806035b55834a8f299bd3e4fe7d27c02e
Aimi-no <naoko.aimi@gmail.com>
2019-04-15 21:53:22 +0200

    Add files via upload

---
+++ decimation_script.py
@@ -0,0 +1,89 @@
+bl_info = {
+    "name": "New Object",
+    "author": "Your Name Here",
+    "version": (1, 0),
+    "blender": (2, 75, 0),
+    "location": "View3D > Add > Mesh > New Object",
+    "description": "Adds a new Mesh Object",
+    "warning": "",
+    "wiki_url": "",
+    "category": "Add Mesh",
+    }
+
+
+import bpy
+from bpy.types import Operator
+from bpy.props import FloatVectorProperty
+from bpy_extras.object_utils import AddObjectHelper, object_data_add
+from mathutils import Vector
+
+
+def add_object(self, context):
+    bpy.ops.import_mesh.ply(filepath="C:/Users/Aimi/Downloads/meshed-poisson.ply")
+    
+    for obj in bpy.data.objects:
+        if obj.type == "MESH":
+            modDec = obj.modifiers.new("Decimate", type = "DECIMATE")
+            bpy.context.scene.update()
+            #len(obj.data.vertices) 
+
+            while modDec.face_count >= 20000:
+                modDec.ratio *= 0.5
+                bpy.context.scene.update()
+                
+                
+    bpy.ops.export_mesh.ply(filepath="C:/Users/Aimi/Downloads/decimated.ply", use_normals=False, use_uv_coords=False)
+
+
+class OBJECT_OT_add_object(Operator, AddObjectHelper):
+    """Create a new Mesh Object"""
+    bl_idname = "mesh.add_object"
+    bl_label = "Add Mesh Object"
+    bl_options = {'REGISTER', 'UNDO'}
+
+    scale = FloatVectorProperty(
+            name="scale",
+            default=(1.0, 1.0, 1.0),
+            subtype='TRANSLATION',
+            description="scaling",
+            )
+
+    def execute(self, context):
+
+        add_object(self, context)
+
+        return {'FINISHED'}
+
+
+# Registration
+
+def add_object_button(self, context):
+    self.layout.operator(
+        OBJECT_OT_add_object.bl_idname,
+        text="Add Object",
+        icon='PLUGIN')
+
+
+# This allows you to right click on a button and link to the manual
+def add_object_manual_map():
+    url_manual_prefix = "https://docs.blender.org/manual/en/dev/"
+    url_manual_mapping = (
+        ("bpy.ops.mesh.add_object", "editors/3dview/object"),
+        )
+    return url_manual_prefix, url_manual_mapping
+
+
+def register():
+    bpy.utils.register_class(OBJECT_OT_add_object)
+    bpy.utils.register_manual_map(add_object_manual_map)
+    bpy.types.INFO_MT_mesh_add.append(add_object_button)
+
+
+def unregister():
+    bpy.utils.unregister_class(OBJECT_OT_add_object)
+    bpy.utils.unregister_manual_map(add_object_manual_map)
+    bpy.types.INFO_MT_mesh_add.remove(add_object_button)
+
+
+if __name__ == "__main__":
+    register()
